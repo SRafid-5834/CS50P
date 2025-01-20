f3da574114eaf99e38ca48f964fe45c1fa67aede
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-01-19 23:42:53 -0330

    indoor

---
+++ Week 0/indoor.py
@@ -0,0 +1,12 @@
+# Indoor Voice
+
+def main():
+    user_input = answer("Enter your answer here: ")
+    print(change_voice(user_input))
+
+
+def change_voice(sentence):
+    return sentence.lower()
+
+
+main()
