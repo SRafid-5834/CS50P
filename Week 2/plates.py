0ceed11f096ab656f1814dbe5f002938e5835f69
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-01-21 23:42:43 -0330

    plates

---
+++ Week 2/plates.py
@@ -0,0 +1,35 @@
+# Vanity Plates
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if s.isalnum() and (1 < len(s) < 7) and s[:2].isalpha():
+        if len(s) == 2:
+            return True
+        elif len(s) > 2:
+            t = s[2:]
+            for i in range(len(t)):
+                if t[i].isalpha():
+                    pass
+                elif t[i].isdigit():
+                    if t[i] == '0':
+                        return False
+                    elif t[i:].isdigit():
+                        return True
+                    else:
+                        return False
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    main()
