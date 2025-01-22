c72fa09bda6b0ea4dd384824cc6256a9e39c9cb4
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-01-21 23:36:20 -0330

    coke

---
+++ Week 2/coke.py
@@ -0,0 +1,14 @@
+# Coke Machine
+
+def main():
+    amount_due = 50
+    while amount_due > 0:
+        print(f'Amount Due: {amount_due}')
+        coin = int(input("Insert Coin: "))
+        if coin in [25, 10, 5]:
+            amount_due -= coin            
+    print(f'Change Owed: {-amount_due}')
+
+
+if __name__ == "__main__":
+    main()
