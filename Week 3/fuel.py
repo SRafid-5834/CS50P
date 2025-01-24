695e5f724df8e93d8eb89214012dae4faccb4971
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-01-23 23:41:09 -0330

    fuel

---
+++ Week 3/fuel.py
@@ -0,0 +1,24 @@
+def main():
+    val = round(process_input("Fraction: ") * 100)
+
+    if val <= 1:
+        print("E")
+    elif val >= 99:
+        print("F")
+    else:
+        print(f'{val}%')
+
+
+def process_input(prompt):
+    while True:
+        x = input(prompt).split("/")
+        print(int(x[0]) / int(x[1]))
+        try:
+            if not (int(x[0]) > int(x[1])):
+                return int(x[0]) / int(x[1])
+        except (ValueError, ZeroDivisionError):
+            pass
+
+
+if __name__ == '__main__':
+    main()
