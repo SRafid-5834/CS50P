06ab859d1b817583c803fa72ff4b4df3e181890c
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-01-24 23:06:57 -0330

    emojize

---
+++ Week 4/emojize.py
@@ -0,0 +1,12 @@
+# Emojize
+
+import emoji
+
+
+def main():
+    user_input = input("Input: ")
+    print(f"Output: {emoji.emojize(user_input, language='alias')}")
+
+
+if __name__ == '__main__':
+    main()
