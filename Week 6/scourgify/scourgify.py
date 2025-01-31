414362a3ec0441c41d67d23f43f266dde6554153
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-01-30 23:54:56 -0330

    scourgify

---
+++ Week 6/scourgify/scourgify.py
@@ -0,0 +1,27 @@
+import csv
+import sys
+
+
+def main():
+
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+    if not sys.argv[1].endswith(".csv"):
+        sys.exit("Not a CSV file")
+
+    try:
+        with open(sys.argv[1]) as file_1, open(sys.argv[2], "w", newline="") as file_2:
+            reader = csv.DictReader(file_1)
+            writer = csv.DictWriter(file_2, fieldnames=["first", "last", "house"])
+            writer.writeheader()
+            for row in reader:
+                last, first = row["name"].strip().split(", ")
+                writer.writerow({"first": first, "last": last, "house": row["house"]})
+    except FileNotFoundError:
+        sys.exit(f"Could not read {sys.argv[1]}")
+
+
+if __name__ == '__main__':
+    main()
